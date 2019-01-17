7288913324b2d74a62828d0baafb9ba18eab030e
ebcrowder <ebcrowder@gmail.com>
2019-01-16 21:23:02 -0800

    user creation mutation and queries

--- api/src/resolvers.ts
+++ api/src/resolvers.ts
@@ -1,10 +1,13 @@
 import { forwardTo } from 'prisma-binding';
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 export const resolvers = {
   Query: {
     trips: forwardTo('prisma'),
     trip: forwardTo('prisma'),
-    tripsConnection: forwardTo('prisma')
+    tripsConnection: forwardTo('prisma'),
+    users: forwardTo('prisma')
   },
   Mutation: {
     createTrip(_, args, context, info) {
@@ -28,9 +31,37 @@ export const resolvers = {
         }
       });
     },
-    async deleteTrip(_, args, context, info) {
+    deleteTrip(_, args, context, info) {
       const where = { id: args.id };
       return context.prisma.mutation.deleteTrip({ where });
+    },
+    async signup(_, args, context, info) {
+      // lowercase their email
+      args.email = args.email.toLowerCase();
+      // hash their password
+      const password = await bcrypt.hash(args.password, 10);
+      // create the user in the database
+      const user = await context.prisma.mutation.createUser(
+        {
+          data: {
+            ...args,
+            password,
+            permissions: { set: ['ADMIN'] }
+          }
+        },
+        info
+      );
+
+      // create the JWT token for them
+      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+      // We set the jwt as a cookie on the response
+      context.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+      });
+
+      // return the user to the browser
+      return user;
     }
   }
 };
