b574ff8bf87ae8fadc179ac2a2022bf143f13883
ebcrowder <ebcrowder@gmail.com>
2019-01-22 20:06:27 -0800

    add signin page and updated resolvers

--- api/src/resolvers.ts
+++ api/src/resolvers.ts
@@ -52,16 +52,29 @@ export const resolvers = {
         info
       );
 
-      // create the JWT token for them
-      const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-      // We set the jwt as a cookie on the response
-      context.response.cookie('token', token, {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
-      });
+      // create the JWT token for them and return the user to the browser
+      return {
+        token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+        user
+      };
+    },
+    async signin(_, { email, password }, context, info) {
+      // 1. check if there is a user with that email
+      const user = await context.prisma.query.user({ where: { email } });
+      if (!user) {
+        throw new Error(`No such user found for email ${email}`);
+      }
+      // 2. Check if their password is correct
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        throw new Error('Invalid Password!');
+      }
 
-      // return the user to the browser
-      return user;
+      // create the JWT token for them and return the user to the browser
+      return {
+        token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+        user
+      };
     }
   }
 };
