39bf07fd4e347bbc611d87c21484ceccf89ec7b2
ebcrowder <ebcrowder@gmail.com>
2019-02-09 18:32:27 -0800

    almost

--- client/components/Cards.tsx
+++ client/components/Cards.tsx
@@ -18,40 +18,53 @@ interface CardsProps {
 }
 
 class Cards extends React.Component<CardsProps, {}> {
-  filterProps = trips =>
+  public state = {
+    tripData: []
+  };
+
+  public componentDidMount() {
+    const tripData = this.props.ALL_TRIPS_QUERY.trips;
+    this.setState({ tripData });
+  }
+
+  public filterProps = trips =>
     trips.filter(item => item.dream || item.pastEvent || item.upcomingEvent);
 
-  componentDidUpdate(prevProps) {
-    const tripArray = [];
-    console.log(prevProps.ALL_TRIPS_QUERY);
+  public componentDidUpdate(prevProps) {
+    const tripData = [];
+
     if (this.props.isDream !== prevProps.isDream) {
-      this.props.ALL_TRIPS_QUERY.refetch().then(query =>
-        tripArray.push(this.filterProps(query.data.trips))
-      );
-    } else if (this.props.isUpcoming !== prevProps.isUpcoming) {
-      this.props.ALL_TRIPS_QUERY.refetch().then(query =>
-        tripArray.push(this.filterProps(query.data.trips))
-      );
-    } else if (this.props.isPast !== prevProps.isPast) {
-      this.props.ALL_TRIPS_QUERY.refetch().then(query =>
-        tripArray.push(this.filterProps(query.data.trips))
-      );
+      // this.props.ALL_TRIPS_QUERY.refetch()
+      //   .then(query => query.data.trips)
+      //   .then(data => tripData.push(this.filterProps(data)));
+
+      this.props.ALL_TRIPS_QUERY.refetch()
+        .then(query => console.log(query.data.trips))
+        .then(data => console.log(this.filterProps(data)));
     }
-    return tripArray;
+    // else if (this.props.isUpcoming !== prevProps.isUpcoming) {
+    //   this.props.ALL_TRIPS_QUERY.refetch().then(query =>
+    //     tripArray.push(this.filterProps(query.data.trips))
+    //   );
+    // } else if (this.props.isPast !== prevProps.isPast) {
+    //   this.props.ALL_TRIPS_QUERY.refetch().then(query =>
+    //     tripArray.push(this.filterProps(query.data.trips))
+    //   );
+    // }
+    // return this.setState({ tripData });
   }
 
   public render() {
-    const { isDream, isUpcoming, isPast } = this.props;
+    const { tripData } = this.state;
 
     return (
       <Query query={ALL_TRIPS_QUERY}>
         {({ data, loading }) => {
-          const filterData = this.filterProps(data.trips);
           if (loading) return <p>Loading...</p>;
           return (
             <>
               <CardsList>
-                {filterData.map(item => (
+                {tripData.map(item => (
                   <Card trip={item} key={item.id} />
                 ))}
               </CardsList>
